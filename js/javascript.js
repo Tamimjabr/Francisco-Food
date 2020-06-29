f6103b6685886429ea396264262859a03a700b94
T.J <Tamimjabr@github.com>
2020-06-29 21:06:13 +0200

    editing

--- js/javascript.js
+++ js/javascript.js
@@ -4,7 +4,7 @@ $(function(){
         $(".sideNav").animate({width:"+=250px"}, 500);
     });
     //closing the menu
-    $(".closeBtn").click(function(){
+    $("#closeBtn").click(function(){
         $(".sideNav").animate({width:"-=250px"}, 500);
     });
 });
